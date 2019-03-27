5dab9cbb26e4a4087c4c665d6b570771ccde16e7
OstapHetman <prickly4528@gmail.com>
2019-03-27 12:43:26 +0200

    implement get,update,delete routes

--- server/app.js
+++ server/app.js
@@ -27,7 +27,6 @@ app.get('/api/create/postTable', (req, res) => {
 
   db.query(sql, (err, result) => {
     if (err) throw err;
-    console.log(result);
     res.send('Posts table created...')
   })
 });
@@ -39,8 +38,44 @@ app.post('/api/create/post', (req, res) => {
   let sql = 'INSERT INTO posts SET ?';
   let query = db.query(sql, post, (err, result) => {
     if (err) throw err;
-    console.log(result);
     res.send('Post Added');
+  });
+})
+
+// Get all posts
+app.get('/api/get/posts', (req, res) => {
+  let sql = 'SELECT * FROM posts';
+  let query = db.query(sql, (err, results) => {
+    if (err) throw err;
+    res.send('Posts Fetched');
+  });
+})
+
+// Get single post
+app.get('/api/get/post/:id', (req, res) => {
+  let sql = `SELECT * FROM posts WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send('Post Fetched');
+  });
+})
+
+// Update post
+app.post('/api/update/post/:id', (req, res) => {
+  let data = { title: req.body.title, body: req.body.body };
+  let sql = `UPDATE posts SET title = '${data.title}', body = '${data.body}' WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send('Post Updated');
+  });
+})
+
+// Delete post
+app.get('/api/delete/post/:id', (req, res) => {
+  let sql = `DELETE FROM posts WHERE id = ${req.params.id}`;
+  let query = db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.send('Post Deleted');
   });
 })
 
